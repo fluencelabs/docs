bd7291268e0d8859d1178308cfda8ade0ab0c998
Artsiom Shamsutdzinau <shamsartem@gmail.com>
2023-02-28 12:43:35 +0100

    attempt 5

--- static/mermaid.js
+++ static/mermaid.js
@@ -6,27 +6,42 @@ const loadMermaid = () => {
 
 // @ts-check
 
+const replaceWithMermaid = (el) => {
+  const pre = document.createElement("pre");
+  pre.classList.add("mermaid");
+  pre.textContent = el.children[0].children[0].textContent;
+  el.replaceWith(pre);
+};
+
 setInterval(() => {
   const shikiElements = Array.from(document.getElementsByClassName("shiki"));
   shikiElements.forEach((el) => el.classList.add("visible"));
-  const replacedElements = shikiElements
-    .filter((el) => !el.children[0].classList.contains("language-id"))
-    .map((el) => {
-      if (el.classList.contains("min-dark")) {
-        return el.remove();
-      }
+  const elementsToReplace = shikiElements.filter(
+    (el) => !el.children[0].classList.contains("language-id")
+  );
 
-      const pre = document.createElement("pre");
-      pre.classList.add("mermaid");
-      pre.textContent = el.children[0].children[0].textContent;
-      el.after(pre);
-      requestAnimationFrame(() => {
+  elementsToReplace.forEach((el) => {
+    if (el.classList.contains("min-dark")) {
+      el.remove();
+      return;
+    }
+
+    replaceWithMermaid(el);
+  });
+  const setOfDiagrams = new Set();
+  Array.from(document.getElementsByClassName("mermaid"))
+    .filter((el) => el.children[0]?.classList.has("code-container"))
+    .forEach((el) => {
+      const textContent = el.children[0].children[0].textContent;
+      if (setOfDiagrams.has(textContent)) {
         el.remove();
-      });
-      return el;
+        return;
+      }
+      replaceWithMermaid(el);
+      setOfDiagrams.add(textContent);
     });
 
-  if (replacedElements.length > 0) {
+  if (elementsToReplace.length > 0) {
     loadMermaid();
   }
 }, 60);
